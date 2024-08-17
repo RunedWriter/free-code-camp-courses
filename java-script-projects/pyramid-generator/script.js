7ce7120ceb50987a31dbf1e7a76fa078f93edda6
RunedWriter <ptriley2016@gmail.com>
2024-08-17 01:10:16 -0400

    Steps 26-18 Learning methods. push and pop so far push to push the last item out of an array pop to return that value

--- java-script-projects/pyramid-generator/script.js
+++ java-script-projects/pyramid-generator/script.js
@@ -1,4 +1,8 @@
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
+let pushed = rows.push(pushed);
+console.log(pushed);
+let popped = rows.pop();
+console.log(popped);
 console.log(rows);
