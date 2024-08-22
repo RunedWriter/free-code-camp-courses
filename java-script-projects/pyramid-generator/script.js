8b3cded976a57c05df31ff1bec3b1feb9d384a72
RunedWriter <ptriley2016@gmail.com>
2024-08-21 21:57:15 -0400

    Step 70 Change character.repeat value to 2 * rowNumber - 1 to make the pyramid symetrical

--- java-script-projects/pyramid-generator/script.js
+++ java-script-projects/pyramid-generator/script.js
@@ -4,7 +4,7 @@ const rows = [];
 
 function padRow(rowNumber, rowCount){
     return " ".repeat(rowCount - rowNumber) 
-    + character.repeat(rowNumber) 
+    + character.repeat(2 * rowNumber - 1) 
     + " ".repeat(rowCount - rowNumber);
 }
 
