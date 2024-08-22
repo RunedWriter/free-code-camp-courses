2470b542dad95a50ebebb43d6dfa3819843b5244
RunedWriter <ptriley2016@gmail.com>
2024-08-21 22:29:31 -0400

    Step 78 Make if statement that logs string

--- java-script-projects/pyramid-generator/script.js
+++ java-script-projects/pyramid-generator/script.js
@@ -12,6 +12,9 @@ function padRow(rowNumber, rowCount){
 /*for (let i = 1; i <= count; i ++) {
     rows.push(padRow(i, count));
 }*/
+if (true) {
+    console.log("Condition is true")
+  }
 
 let result = [""];
 
